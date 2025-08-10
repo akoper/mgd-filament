bb56dab17a7583ed5b02ebaca034a9cc65292cb0
andrew_koper <andrew.koper@gmail.com>
2025-08-10 19:14:12 -0400

    Create Organization CRUD with Filament

---
+++ app/Filament/Resources/Organizations/Tables/OrganizationsTable.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filament\Resources\Organizations\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Table;
+
+class OrganizationsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
