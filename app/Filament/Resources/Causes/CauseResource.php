c2b07462736a3223226f652e7ab9db27268dfd0d
andrew_koper <andrew.koper@gmail.com>
2025-08-10 19:28:35 -0400

    Create Cause CRUD with Filament

---
+++ app/Filament/Resources/Causes/CauseResource.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Filament\Resources\Causes;
+
+use App\Filament\Resources\Causes\Pages\CreateCause;
+use App\Filament\Resources\Causes\Pages\EditCause;
+use App\Filament\Resources\Causes\Pages\ListCauses;
+use App\Filament\Resources\Causes\Schemas\CauseForm;
+use App\Filament\Resources\Causes\Tables\CausesTable;
+use App\Models\Cause;
+use BackedEnum;
+use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use Filament\Tables\Table;
+
+class CauseResource extends Resource
+{
+    protected static ?string $model = Cause::class;
+
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+
+    protected static ?string $recordTitleAttribute = 'name';
+
+    public static function form(Schema $schema): Schema
+    {
+        return CauseForm::configure($schema);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return CausesTable::configure($table);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => ListCauses::route('/'),
+            'create' => CreateCause::route('/create'),
+            'edit' => EditCause::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/Causes/Schemas/CauseForm.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\Causes\Schemas;
+
+use Filament\Schemas\Schema;
+
+class CauseForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+            ]);
+    }
+}
