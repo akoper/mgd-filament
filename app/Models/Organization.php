8eaf8d9b24e92ad53362937c2af86539318e6876
andrew_koper <andrew.koper@gmail.com>
2025-08-15 14:51:36 -0400

    Updated Organization migration and model

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -3,15 +3,40 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Organization extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'phone',
+        'street',
+        'city',
+        'state',
+        'zip_code',
+        'mission_statement',
+        'admin_user_id'
+    ];
+
     /**
      * Get the users that belong to the organization.
      */
     public function users(): HasMany
     {
         return $this->hasMany(User::class, 'org_id');
+    }
+
+    /**
+     * Get the admin user that manages the organization.
+     */
+    public function admin(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'admin_user_id');
     }
 }
